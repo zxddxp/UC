535ee21f30f6543a8ce1de873e213623abbebe8b
zxddxp <zxddxp@gmail.com>
2013-03-23 14:38:11 -0700

    two problems done

---
+++ src/ride.java
@@ -0,0 +1,35 @@
+/*
+ID: zxddxp1
+LANG: JAVA
+TASK: ride
+*/
+import java.io.*;
+import java.util.*;
+
+public class ride {
+    public static void main (String [] args) throws IOException {
+        // Use BufferedReader rather than RandomAccessFile; it's much faster
+        BufferedReader f = new BufferedReader(new FileReader("ride.in"));
+        // input file name goes above
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
+        // Use StringTokenizer vs. readLine/split -- lots faster
+        String st = f.readLine().trim();
+        int i1 = 1;
+        for (int i = 0; i < st.length(); i++) {
+            i1 *= (int) (st.charAt(i) - 'A' + 1);
+        }
+        st = f.readLine().trim();
+        int i2 = 1;
+        for (int i = 0; i < st.length(); i++) {
+            i2 *= (int) (st.charAt(i) - 'A' + 1);
+        }
+        if (i1 % 47 == i2 % 47) {
+            out.println("GO");
+        }
+        else {
+            out.println("STAY");
+        }
+        out.close();                                  // close the output file
+        System.exit(0);                               // don't omit this!
+    }
+}
